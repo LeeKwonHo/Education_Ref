bf43eeb1694dabda16ce83b2c547e396b656ec22
LeeKwonHo <yaaa3717@naver.com>
2023-09-11 17:46:14 +0900

    Education Constructor

---
+++ REF_EDUCATION/src/refeducation/PersonInformation.java
@@ -0,0 +1,14 @@
+package refeducation;
+
+public class PersonInformation {
+
+	String nation = "대한민국";
+	String name;
+	int age;
+	
+	public PersonInformation(String name, int age) {
+		this.name = name;//this로 초기화할 변수 지정
+		this.age = age;
+	}
+	
+}
---
+++ REF_EDUCATION/src/refeducation/PersonTester.java
@@ -0,0 +1,18 @@
+package refeducation;
+
+public class PersonTester {
+
+	public static void main(String[] args) {
+		PersonInformation Ps = new PersonInformation("이권호", 23);
+		System.out.println(Ps.nation + "출생");
+		System.out.println(Ps.name + "님");
+		System.out.println(Ps.age + "세");
+		
+		PersonInformation ps2 = new PersonInformation("강성식", 23);
+		System.out.println(ps2.nation + "출생");
+		System.out.println(ps2.name + "님");
+		System.out.println(ps2.age + "세");
+
+	}
+
+}
